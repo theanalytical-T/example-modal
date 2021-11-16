ec60f142c7225c5c94331d6061ef468eb54dec81
Tiara Brunt <53661450+tiara143@users.noreply.github.com>
2021-11-16 11:50:50 -0600

    11/16/2021
    Completed this short example of a modal using html, scss, and js while using classes with the querySelector instead of ids with getElementById.

---
+++ Short Modal Example/dist/app.js
@@ -0,0 +1,7 @@
+document.querySelector(".open-modal").addEventListener("click", () => {
+    document.querySelector(".overlay").style.display = "block";
+})
+
+document.querySelector(".close-modal").addEventListener("click", () => {
+    document.querySelector(".overlay").style.display = "none";
+})
